f8e44d8496d952e8fbcc92607612d60f875c2937
Ronaldo <20222tadssaj0007@ifba.edu.br>
2025-06-22 15:27:06 -0300

    att pom

--- src/main/java/com/system/clinic/consts/RequestPathConstants.java
+++ src/main/java/com/system/clinic/consts/RequestPathConstants.java
@@ -6,9 +6,10 @@ public class RequestPathConstants {
     }
 
     public static final String HOME = "/home";
-    public static final String VETERINARIO = "/veterinario";
-    public static final String DONO = "/dono";
-    public static final String PET = "/pet";
+    public static final String LOGIN = "/login";
+    public static final String LOGOUT = "/logout";
+    public static final String CADASTRO = "/cadastro";
+    public static final String PACIENTE = "/paciente";
     public static final String CONSULTA = "/consulta";
 
 }
--- src/main/java/com/system/clinic/controller/LoginController.java
+++ src/main/java/com/system/clinic/controller/LoginController.java
@@ -2,8 +2,11 @@ package com.system.clinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static com.system.clinic.consts.RequestPathConstants.LOGIN;
 
 @Controller
+@RequestMapping(LOGIN)
 public class LoginController {
 
     @GetMapping("/login")
