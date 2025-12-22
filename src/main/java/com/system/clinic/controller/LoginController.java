d94837e7170f7f7230051c9e07d7d810c0e350d4
Ronaldo-Correia <20222tadssaj0007@ifba.edu.br>
2025-12-22 18:55:33 -0300

    add Msg error in login and att css

--- src/main/java/com/system/clinic/controller/LoginController.java
+++ src/main/java/com/system/clinic/controller/LoginController.java
@@ -1,14 +1,16 @@
 package com.system.clinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; // não esqueça de importar!
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public String login(@RequestParam(required = false) String error, Model model) {
+        model.addAttribute("error", error != null);
+        return "login"; 
     }
-
 }
